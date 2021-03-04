6e929f3d9666e54e0d1b845cd8e20d46d6087d97
chal2and <chal2and@gmail.com>
2021-03-04 11:02:48 +0900

    first

---
+++ src/main/kotlin/kr/co/chal2and/bluemarble/plugin/BlueMarblePlugin.kt
@@ -0,0 +1,13 @@
+package kr.co.chal2and.bluemarble.plugin
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BlueMarblePlugin : JavaPlugin(){
+    override fun onEnable() {
+        //Esnable
+    }
+
+    override fun onDisable() {
+        //Disable
+    }
+}
